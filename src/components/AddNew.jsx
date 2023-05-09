0e4f69711441b6f2a0ac96e31501ee0c8799f085
Wiktoria Rekić <wiktoriarekic@gmail.com>
2023-05-09 23:51:56 +0200

    added negative numbers validation

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState, useRef, useEffect } from 'react'
-import {Link} from 'react-router-dom'
 import DataContext from '../context/DataContext'
 
 export default function AddNew(){
@@ -12,6 +11,7 @@ export default function AddNew(){
     },[])
 
     const [propertyData, setPropertyData] = useState({name: "", city: "", bedrooms: "", price: "", desc: ""})
+    const [isSubmitted, setIsSubmitted] = useState(false)
 
     const handleChange = event => {
         const category = event.target.getAttribute("data-type")
@@ -23,9 +23,30 @@ export default function AddNew(){
         setPropertyData(current => ({...current, [category]: value}))
     } 
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
         addTour({...propertyData, img: `https://picsum.photos/seed/${Math.round(Math.random()*10000)}/1920/1080`, id: (Math.round(Math.random()*100000)).toString()})
+        
+        setIsSubmitted(true)
     }
+
+    const formArea = !isSubmitted ? <>
+            <form onSubmit={handleSubmit} className="hero-form">
+                <div className="login-area">
+                    <label className="paragraph login-label" htmlFor="name">Address</label>
+                    <input data-type="name" className="login-input" id="name" type="text" required onChange={handleChange}></input>
+                    <label className="paragraph login-label" htmlFor="city">City</label>
+                    <input data-type="city" className="login-input" id="city" type="text" required onChange={handleChange}></input>
+                    <label className="paragraph login-label" htmlFor="bedrooms">Number of bedrooms</label>
+                    <input data-type="bedrooms" className="login-input" id="bedrooms" type="number" min="0" required onChange={handleChange}></input>
+                    <label className="paragraph login-label" htmlFor="price">Price in $</label>
+                    <input data-type="price" className="login-input" id="price" type="number" min="0" required onChange={handleChange}></input>
+                    <label className="paragraph login-label" htmlFor="desc">Description</label>
+                    <textarea data-type="desc" className="login-input add-ta" id="desc" type="text-area" required onChange={handleChange}></textarea>
+                </div>
+                <button className="btn btn-yellow btn-post" type="submit">Post your offer</button>
+            </form>
+        </> : <h1 className='title'>Offer posted successfully!</h1>
 
     return(
         <>
@@ -34,24 +55,7 @@ export default function AddNew(){
                     <h1 className="title">Want to post a new offer?</h1>
                     <p className="paragraph">Fill in your property's data below!</p>
                 </div>
-                <form className="hero-form">
-                    <div className="login-area">
-                        <label className="paragraph login-label" htmlFor="name">Address</label>
-                        <input data-type="name" className="login-input" id="name" type="text" required onChange={handleChange}></input>
-                        <label className="paragraph login-label" htmlFor="city">City</label>
-                        <input data-type="city" className="login-input" id="city" type="text" required onChange={handleChange}></input>
-                        <label className="paragraph login-label" htmlFor="bedrooms">Number of bedrooms</label>
-                        <input data-type="bedrooms" className="login-input" id="bedrooms" type="number" required onChange={handleChange}></input>
-                        <label className="paragraph login-label" htmlFor="price">Price in $</label>
-                        <input data-type="price" className="login-input" id="price" type="number" required onChange={handleChange}></input>
-                        <label className="paragraph login-label" htmlFor="desc">Description</label>
-                        <textarea data-type="desc" className="login-input add-ta" id="desc" type="text-area" required onChange={handleChange}></textarea>
-                    </div>
-                    
-                    <Link onClick={handleSubmit} to='/'>
-                        <button className="btn btn-yellow btn-post" type="submit">Post your offer</button>
-                    </Link>
-                </form>
+                {formArea}
             </section>
         </>
     )
